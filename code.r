68e23cea5a2dcc42332d5a17ce380c9a526832df
unknown <afiqahamalia02@gmail.com>
2023-05-13 16:59:34 +0700

    Pra proses

--- code.r
+++ code.r
@@ -1,6 +1,8 @@
 #Baca data
 data <- read.csv("marketing_campaign.csv", sep = "\t", header=TRUE)
 
+###PRAPROSES DATA###
+
 #Membuat atribut age untuk mengetahui usia customer
 data$Age <- NULL
 data$Age <- 2023-data$Year_Birth
@@ -20,39 +22,60 @@ now <- as.Date('1-5-2023',format='%d-%m-%Y')
 data$Dt_Customer <- as.Date(data$Dt_Customer, format='%d-%m-%Y')
 data$Seniority <- as.integer(now - data$Dt_Customer)
 
-#Memberi kode pada atribut Education
-#1=undergraduated; 2=postgraduated
-data$Education[which(data$Education=="Basic")] <- 1
-data$Education[which(data$Education=="2n Cycle")] <- 1
-data$Education[which(data$Education=="Graduation")] <- 2
-data$Education[which(data$Education=="Master")] <- 2
-data$Education[which(data$Education=="PhD")] <- 2
-data$Education <- as.integer(data$Education)
-
-#Memberi kode pada atribut Marital Status
-#1=alone; 2=couple
-data$Marital_Status[which(data$Marital_Status=="Divorced")] <- 1
-data$Marital_Status[which(data$Marital_Status=="Single")] <- 1
-data$Marital_Status[which(data$Marital_Status=="Absurd")] <- 1
-data$Marital_Status[which(data$Marital_Status=="Widow")] <- 1
-data$Marital_Status[which(data$Marital_Status=="YOLO")] <- 1
-data$Marital_Status[which(data$Marital_Status=="Married")] <- 2
-data$Marital_Status[which(data$Marital_Status=="Together")] <- 2
-data$Marital_Status <- as.integer(data$Marital_Status)
-
 #Memilih atribut yang diperlukan
-dataCust <- data.frame(data$Age, data$Education, data$Marital_Status, data$Income, data$Spending)
+dataCust <- data.frame(data$Age, data$Marital_Status, data$Children, data$Education, data$Seniority,
+                       data$Income, data$Spending, data$MntWines, data$MntFruits, data$MntMeatProducts,
+                       data$MntFishProducts, data$MntSweetProducts, data$MntGoldProds)
 
+#Memilih Marital_Status yang bernilai Single dan Married
+dataCust <- subset.data.frame(dataCust, data.Marital_Status=="Single" | data.Marital_Status=="Married")
+
+#Pembersihan Data
 #Cek missing value
 library(mice)
 md.pattern(dataCust)
 
-#Mengisi missing
-library("tidyr")
-dataCust <- dataCust %>% drop_na()
-
-#Cek missing value
+#Isi nilai missing value
+dataCust$data.Income[is.na(dataCust$data.Income)] <- mean(dataCust$data.Income, na.rm= TRUE)
 md.pattern(dataCust)
 
-#Hapus outliers
-dataCust <- subset(dataCust, dataCust$data.Income < 600000)
+
+str(dataCust)
+
+#Inisialisasi Data Kategorik
+#Atribut Education
+#1=undergraduated; 2=postgraduated
+dataCust$data.Education[which(dataCust$data.Education=="Basic")] <- 1
+dataCust$data.Education[which(dataCust$data.Education=="2n Cycle")] <- 1
+dataCust$data.Education[which(dataCust$data.Education=="Graduation")] <- 2
+dataCust$data.Education[which(dataCust$data.Education=="Master")] <- 2
+dataCust$data.Education[which(dataCust$data.Education=="PhD")] <- 2
+dataCust$data.Education <- as.integer(dataCust$data.Education)
+
+#Atribut Marital Status
+#1=Single; 2=Married
+dataCust$data.Marital_Status[which(dataCust$data.Marital_Status=="Single")] <- 1
+dataCust$data.Marital_Status[which(dataCust$data.Marital_Status=="Married")] <- 2
+dataCust$data.Marital_Status <- as.integer(dataCust$data.Marital_Status)
+
+
+summary(dataCust)
+
+#Normalisasi
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+dataCust$data.Age <- normalize(dataCust$data.Age)
+dataCust$data.Marital_Status <- normalize(dataCust$data.Marital_Status)
+dataCust$data.Children <- normalize(dataCust$data.Children)
+dataCust$data.Education <- normalize(dataCust$data.Education)
+dataCust$data.Seniority <- normalize(dataCust$data.Seniority)
+dataCust$data.Income <- normalize(dataCust$data.Income)
+dataCust$data.Spending <- normalize(dataCust$data.Spending)
+dataCust$data.MntWines <- normalize(dataCust$data.MntWines)
+dataCust$data.MntFruits <- normalize(dataCust$data.MntFruits)
+dataCust$data.MntMeatProducts <- normalize(dataCust$data.MntMeatProducts)
+dataCust$data.MntFishProducts <- normalize(dataCust$data.MntFishProducts)
+dataCust$data.MntSweetProducts <- normalize(dataCust$data.MntSweetProducts)
+dataCust$data.MntGoldProds <- normalize(dataCust$data.MntGoldProds)
